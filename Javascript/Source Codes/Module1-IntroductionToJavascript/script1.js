4b7c341339eb5da8ba7213a98fcb13422fe76659
Sagnik Chakraborty <official.sagnikchakraborty2@gmail.com>
2024-08-02 22:20:39 +0530

    Module 1

---
+++ Javascript/Source Codes/Module1-IntroductionToJavascript/script1.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log("Hello World", 100);
+
+console.error("This is an error message");
+console.warn("This is a warning message");
+
+console.table({name:"Sagnik",profession:"Software Engineer"})
